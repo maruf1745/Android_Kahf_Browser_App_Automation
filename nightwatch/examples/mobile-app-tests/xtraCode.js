9bcb41c772caea6997629c5757ec787d240ad9c7
unknown <marufuzzaman7@gmail.com>
2024-11-15 16:50:20 +0600

    Add Capabilites

---
+++ nightwatch/examples/mobile-app-tests/xtraCode.js
@@ -0,0 +1,9 @@
+//Capabilities
+{
+    "platformName": "Android",
+    "appium:platformVersion": "11",
+    "appium:deviceName": "SKDOP500BD421J001967",
+    "appium:automationName": "UiAutomator2",
+    "appium:appWaitForLaunch": false,
+    "appium:noReset": true
+  }
